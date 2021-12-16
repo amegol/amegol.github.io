b435854d6be14c0d0bdc24d920303e53f4ca8b0a
Ezrabro <67427897+Ezrabro@users.noreply.github.com>
2021-12-16 15:07:49 +0330

    audio test final

--- code.js
+++ code.js
@@ -74,7 +74,7 @@ function makeCall() {
         log('Received stream');
         document.getElementById('video').srcObject = remoteStream;
         document.getElementById('video-2').srcObject = stream;
-        document.getElementById('video').muted = true;
+        document.getElementById('video-2').muted = true;
     });
     }, function(err) {
     log('Failed to get local stream ' + err);
